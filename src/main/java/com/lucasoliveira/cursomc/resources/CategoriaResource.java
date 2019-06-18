de55793c40ef81feb5856517c7641c483f9d21c4
Lucas Oliveira <lcsd.lucas@gmail.com>
2019-06-18 20:24:57 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/lucasoliveira/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/lucasoliveira/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.lucasoliveira.cursomc.resources;
 
 import com.lucasoliveira.cursomc.domain.Categoria;
+import com.lucasoliveira.cursomc.domain.dto.CategoriaDTO;
 import com.lucasoliveira.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -45,5 +48,12 @@ public class CategoriaResource {
     public ResponseEntity<Void> delete (@PathVariable Integer id){
         categoriaService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<CategoriaDTO>> findAll(){
+        List<Categoria> list = categoriaService.findAll();
+        List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
--- src/main/java/com/lucasoliveira/cursomc/services/CategoriaService.java
+++ src/main/java/com/lucasoliveira/cursomc/services/CategoriaService.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CategoriaService {
 
@@ -37,5 +39,9 @@ public class CategoriaService {
         }catch(DataIntegrityViolationException e){
             throw new DataIntegrityException("Não foi possível excluir uma categoria que possui produtos");
         }
+    }
+
+    public List<Categoria> findAll(){
+        return categoriaRepository.findAll();
     }
 }
