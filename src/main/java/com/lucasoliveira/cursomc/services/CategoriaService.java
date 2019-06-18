66d6dc761752dc42a47ae8ca38fb203297e9458e
Lucas Oliveira <lcsd.lucas@gmail.com>
2019-06-18 20:06:34 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/lucasoliveira/cursomc/services/CategoriaService.java
+++ src/main/java/com/lucasoliveira/cursomc/services/CategoriaService.java
@@ -2,8 +2,10 @@ package com.lucasoliveira.cursomc.services;
 
 import com.lucasoliveira.cursomc.domain.Categoria;
 import com.lucasoliveira.cursomc.repositories.CategoriaRepository;
+import com.lucasoliveira.cursomc.services.exception.DataIntegrityException;
 import com.lucasoliveira.cursomc.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -27,5 +29,13 @@ public class CategoriaService {
 
     public Categoria update(Categoria categoria){
         return categoriaRepository.save(categoria);
+    }
+
+    public void delete (Integer id){
+        try {
+            categoriaRepository.delete(id);
+        }catch(DataIntegrityViolationException e){
+            throw new DataIntegrityException("Não foi possível excluir uma categoria que possui produtos");
+        }
     }
 }
---
+++ src/main/java/com/lucasoliveira/cursomc/services/exception/DataIntegrityException.java
@@ -0,0 +1,12 @@
+package com.lucasoliveira.cursomc.services.exception;
+
+public class DataIntegrityException extends RuntimeException {
+
+    public DataIntegrityException(String s){
+        super(s);
+    }
+
+    public DataIntegrityException(String s, Throwable cause){
+        super(s, cause);
+    }
+}
