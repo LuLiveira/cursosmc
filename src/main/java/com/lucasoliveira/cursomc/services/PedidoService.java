d0429911797def50663c3a5e6cc69c24b56b87a2
Lucas Oliveira <lcsd.lucas@gmail.com>
2019-06-17 22:19:02 -0300

    Endepoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/lucasoliveira/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.lucasoliveira.cursomc.services;
+
+
+import com.lucasoliveira.cursomc.domain.Pedido;
+import com.lucasoliveira.cursomc.repositories.PedidoRepository;
+import com.lucasoliveira.cursomc.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoService {
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    public Pedido buscar(Integer id){
+
+        Pedido pedido = pedidoRepository.findOne(id);
+        if(pedido == null){
+            throw new ObjectNotFoundException("Objeto não encontrado id: " + id + " Tipo: " + Pedido.class.getName());
+        }
+
+        return pedido;
+    }
+}
