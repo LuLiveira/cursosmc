56c19a554eb1c11c973cd918f5689690caa13df5
Lucas Oliveira <lcsd.lucas@gmail.com>
2019-06-18 20:53:11 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/lucasoliveira/cursomc/CursomcApplication.java
+++ src/main/java/com/lucasoliveira/cursomc/CursomcApplication.java
@@ -44,6 +44,11 @@ public class CursomcApplication implements CommandLineRunner {
 		//Instanciando as categorias
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama mesa banho");
+		Categoria cat4 = new Categoria(null, "Eletrônicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
 
 		//Instanciando os produtos
 		Produto p1 = new Produto(null, "Computador", 2000.00);
@@ -57,7 +62,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.setCategorias(Arrays.asList(cat1, cat2));
 		p3.setCategorias(Arrays.asList(cat1));
 
-		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		categoriaRepository.save(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.save(Arrays.asList(p1, p2, p3));
 
 		//Instanciando os estados
--- src/main/java/com/lucasoliveira/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/lucasoliveira/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import com.lucasoliveira.cursomc.domain.Categoria;
 import com.lucasoliveira.cursomc.domain.dto.CategoriaDTO;
 import com.lucasoliveira.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -55,5 +56,17 @@ public class CategoriaResource {
         List<Categoria> list = categoriaService.findAll();
         List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    public ResponseEntity<Page<CategoriaDTO>> findPage(
+            @RequestParam(name = "page", defaultValue = "0") Integer page,
+            @RequestParam(name = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(name = "orderBy", defaultValue = "nome") String orderBy,
+            @RequestParam(name = "direction", defaultValue = "ASC") String direction
+    ){
+        Page<Categoria> pagi = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> pagiDto = pagi.map(obj -> new CategoriaDTO(obj));
+        return ResponseEntity.ok().body(pagiDto);
     }
 }
