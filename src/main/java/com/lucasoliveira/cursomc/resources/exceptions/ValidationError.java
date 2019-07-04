7bb7515706335ba458eb332a8150ef5ef6b6e703
Lucas Oliveira <lcsd.lucas@gmail.com>
2019-07-03 23:26:28 -0300

    Padronizando formato de exceções

--- src/main/java/com/lucasoliveira/cursomc/resources/exceptions/ValidationError.java
+++ src/main/java/com/lucasoliveira/cursomc/resources/exceptions/ValidationError.java
@@ -7,8 +7,8 @@ public class ValidationError extends StandardError {
 
     private List<FieldMessage> error = new ArrayList<>();
 
-    public ValidationError(Integer status, String menssagem, Long timeStamp) {
-        super(status, menssagem, timeStamp);
+    public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+        super(timestamp, status, error, message, path);
     }
 
     public List<FieldMessage> getErrors() {
